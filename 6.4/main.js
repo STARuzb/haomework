7e6021b96b23e8e1eb430ebf1018213fc70f9da4
STARuzb <150528348+STARuzb@users.noreply.github.com>
2024-01-26 20:35:24 +0500

    Add files via upload

---
+++ 6.4/main.js
@@ -0,0 +1,10 @@
+const div = document.querySelector('div')
+const ul = document.querySelector('ul')
+const li = document.querySelector('li')
+console.log(div.innerHTML);
+
+
+
+function fayzullo(3 * 1) {
+    console.log(fayzullo);
+}
